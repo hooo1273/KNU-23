2a063f380387bbd769e5b0806410bb07082b6d09
hooo1273 <111989007+hooo1273@users.noreply.github.com>
2023-09-21 09:58:08 +0900

    lecture3
    lecture3

---
+++ 20230921/lecture2.cpp
@@ -0,0 +1,24 @@
+#include <stdio.h>
+int main(void) {
+	int sel, num1, num2;
+	printf("1.더하기 2.빼기 3.곱하기 4.나누기");
+	scanf_s("%d", &sel);
+
+	calculator(sel, num1, num2);
+
+	return 0;
+}
+
+void calculator(int selector, int num1, int num2)
+{
+	printf("숫자 1을 입력하시오 : ");
+	scanf_s("%lf", &num1);
+
+	printf("숫자 2를 입력하시오 : ");
+	scanf_s("%lf", &num2);
+
+	if (selector == 1) sum(num1, num2);
+	else if (selector == 2) sub(num1, num2);
+	else if (selector == 3) mul(num1, num2);
+	else if (selector == 4) div(num1, num2);
+}
