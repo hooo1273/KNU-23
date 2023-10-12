4bd1b9091a4333be01868ad34d6189dab882132e
hooo1273 <111989007+hooo1273@users.noreply.github.com>
2023-10-12 10:52:54 +0900

    project5
    swap

---
+++ 20231012/project5.cpp
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+void swap(int* pa, int* pb)
+{
+	int c = *pa;
+	*pa = *pb;
+	*pb = c;
+}
+
+int main(void) {
+	int a = 10;
+	int b = 20;
+
+	printf("%d %d\n", a, b);
+	swap(&a, &b);
+	printf("%d %d\n", a, b);
+
+	return 0;
+}
