ce8c6e872852a4fa91a103dcec4918c734429a66
hooo1273 <111989007+hooo1273@users.noreply.github.com>
2023-10-19 10:16:19 +0900

    project2
    struct

---
+++ 20231019/project2.cpp
@@ -0,0 +1,38 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+struct Student {
+	int sno;
+	char name[10];
+	int score;
+};
+
+int main(void) {
+	int num_students;
+	struct Student* s;
+
+	printf("학생 수 입력: ");
+	scanf_s("%d", &num_students);
+	s = (struct Stduent*)malloc(10 * sizeof(struct Student));
+	
+	for (int i = 0; i < num_students; i++) {
+		printf("학생# %d-%d 학번 입력: ", num_students, i + 1);
+		scanf_s("%d", &(s[i].sno));
+
+		printf("학생# %d-%d 이름 입력: ", num_students, i + 1);
+		scanf_s("%d", s[i].name, 10);
+
+		printf("학생# %d-%d 성적 입력: ", num_students, i + 1);
+		scanf_s("%d", &(s[i].score));
+
+
+	}
+
+	int sum = 0;
+	for (int i = 0; i < num_students; i++) {
+		sum += s[i].s;
+	}
+	printf("d\n", sum);
+	free(s);
+	return 0;
+}
